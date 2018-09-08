bf754b58e726037de2fa8d637611ce44b23d43d9
mdgordillob <mdgordillob@unal.edu.co>
2018-09-08 17:35:18 -0500

    Se suben ejercicios 4 y 5

---
+++ Ejercicio5.py
@@ -0,0 +1,46 @@
+
+# coding: utf-8
+# In[25]:
+
+
+import turtle
+t = turtle.Pen()
+turtle.resizemode("auto")   
+t.speed=1
+lpp=40
+print("Numero de filas")
+n=int(input())
+h=float(0)
+#print("Numero de lados del poligono de la base")
+m=n+2
+t.speed=1
+l=0
+for l in range(0,n):
+    for h in range(0,n):
+        for k in range(0,m):
+            t.pendown()
+            angl=k*(360/m)
+            t.seth(angl)
+            t.forward(lpp)
+            k=k+1
+        t.penup()
+        t.seth(0)
+        t.forward(100)
+        h=h+1
+    l=l+1
+    t.goto(0,0)
+    t.seth(60)
+    t.forward(l*100)
+    t.pendown()
+    s=s-1
+    n=n-1
+    m=m-1
+t.hideturtle()                   
+turtle.done()
+turtle.bye()
+                
+
+
+
+
+
